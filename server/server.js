a4031e95ab2f098de83792db36f0a279eb9aaf45
unknown <10.javierprieto.1a@gmail.com>
2021-12-08 12:07:32 +0100

    change the server.js file to grab the build

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
+
 
 const port = process.env.PORT || 3000;
 
